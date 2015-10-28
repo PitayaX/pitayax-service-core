12c82db40ece0649be491566f92919d3601cffc9
Bruce Jiang <bruce1977@live.com>
2015-10-28 17:29:59 +0800

    Working

--- test/data/test.js
+++ test/data/test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let fs = require('fs');
+let path = require('path');
 let aq = require('../../').aq;
 let data = require('../../').data;
 
@@ -15,35 +17,71 @@ connections.on = (event, conn) => {
 */
 
 connections.on('open', (conn) => {
-    console.log(conn.Connected);
     console.log(`connection ${conn.Name} was opened`);
+    if (conn.Name === 'test1') {
+
+        setTimeout(() => {
+            console.log('start to close connection test1');
+            connections.close('test1');
+        }, 2000);
+    }
+})
+
+connections.on('close', (conn) => {
+    console.log(`connection ${conn.Name} was closed`);
 })
 
 connections.on('disconnected', (conn) => {
-    console.log(conn.Connected);
     console.log(`connection ${conn.Name} was disconnected`);
 })
 
+connections.on('error', (err, conn) => {
+
+    if (err) {
+        console.log(`Get error from ${conn.Name}: ${err.message} (code: ${err.code})`);
+
+        if (err.code === undefined) {
+            //conn.Connection.disconnect();
+            console.log(`Can't create connection for ${conn.Name}.`);
+            connections.remove(conn.Name);
+        }
+        else connections.close(conn.Name);
+    }
+});
+
+//conn1, 2 and 3 has same connection string, so they will share one connection
 let conn1 = connections.create('test1', 'mongodb://usrpx:password@10.10.73.207:27077/pitayax-test');
 let conn2 = connections.create('test2', 'mongodb://usrpx:password@10.10.73.207:27077/pitayax-test');
+let conn3 = connections.create('test3', 'mongodb://usrpx:password@10.10.73.207:27077/pitayax-test');
+let conn4 = connections.create('test4', 'mongodb://usrpx:password22@10.10.73.207:27077/pitayax-test');
+//let conn5 = connections.create('test5', 'mongodb://usrpx:password@10.10.73.207:27078/pitayax-test');
 
-console.log(conn1.Name);
-console.log(conn2.Name);
-
-connections.close('test1');
-//connections.close('test1');
-
+console.log(`names: ${connections.Names}`);
 
 
-//conn2.Connection.close();
+['/schemas/blog.json', '/schemas/northwind.json']
+    .map(file => path.join(__dirname, file))
+    .map(filename => JSON.parse(fs.readFileSync(filename)))
+    .forEach(schemas => connections.appendSchema(schemas));
 
-
-/*
-let script = undefined;
-let args = [];
-
-for(let i = 0; i < process.argv.length; i++) {
-    if (i == 2) script = process.argv[i];
-    if (i > 2) args.push(process.argv[i]);
+for(let key of connections.Schemas.keys()) {
+    console.log(key);
 }
-*/
+
+let dbAdapter = new data.MongoDBAdapter(connections);
+console.log('created adapter of mongo database');
+
+dbAdapter.retrieve('post', {"title": "title"}, {"method": "findOne"})
+    .then(data => {
+        console.log('find one: ' + JSON.stringify(data, null, 2));
+        return dbAdapter.retrieve('post', {}, {"method": "find"});
+    })
+    .then(data => {
+        console.log('list: ' + JSON.stringify(data, null, 2));
+        return dbAdapter.retrieve('post', {}, {"method": "count"});
+    })
+    .then(data => {
+        console.log('count:' + JSON.stringify(data, null, 2));
+    })
+    .catch(err => console.log(`err: ${err.message}`));
+//let schema = fs.re
